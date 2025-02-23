0144e3053a3edca2459c908606ea324a4477c231
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2025-02-23 19:03:52 +0100

    fix: pwa

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,18 +11,27 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: "portrait",
     background_color: "#ffffff",
     theme_color: "#000000",
+    screenshots: [
+      {
+        src: "/screenshot.png",
+      },
+      {
+        src: "/screenshot-wide.png",
+        form_factor: "wide",
+      },
+    ],
     icons: [
       {
         src: "/web-app-manifest-192x192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "maskable",
+        purpose: "any",
       },
       {
         src: "/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "maskable",
+        purpose: "any",
       },
     ],
   };
